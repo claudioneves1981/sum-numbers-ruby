dc36dd849a1103196ad6b457b5e75245066a817e
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-27 21:53:52 -0300

    Desafio Sum Numbers Ruby

--- lib/numbers.rb
+++ lib/numbers.rb
@@ -1,4 +1,10 @@
 class Numbers
   def sum_text(numbers_text)
+    soma = 0
+    texto = numbers_text.split("\n")
+    for i in 0...texto.length do
+      soma = texto[i].to_i + soma
+    end
+   soma
   end
 end
